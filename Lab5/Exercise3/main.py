a2a4455a6acb3f124c35b16d854db1d9aaad3d31
alex99q <alex99q@abv.bg>
2019-03-24 16:11:52 +0200

    Modified some files

--- Lab5/Exercise3/main.py
+++ Lab5/Exercise3/main.py
@@ -1,4 +1,5 @@
-from morse_code import encrypt_to_morse
+from morse_code import encrypt_to_morse, morse_to_sound
+import time
 
 user_input = input("Type in here: ")
 
@@ -12,5 +13,15 @@ with open("input.txt", mode='r', encoding='utf-8') as f:
 #finally:
 #    f.close()
 
-print("Morse from input: " + encrypt_to_morse(user_input))
-print("Morse from file: " + encrypt_to_morse(file_input))
+morse_from_input = encrypt_to_morse(user_input)
+morse_from_file = encrypt_to_morse(file_input)
+
+print("Morse from input: " + morse_from_input)
+print("Morse from file: " + morse_from_file)
+
+choise_for_sound = input("Do you want to hear the morse code (type in \"yes\" or \"no\")?: ")
+
+if choise_for_sound == "yes":
+    morse_to_sound(morse_from_input)
+    time.sleep(1)
+    morse_to_sound(morse_from_file)
--- Lab5/Exercise3/morse_code.py
+++ Lab5/Exercise3/morse_code.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 def encrypt_to_morse(input_str):
     morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.',
                        'D': '-..', 'E': '.', 'F': '..-.',
@@ -14,7 +17,7 @@ def encrypt_to_morse(input_str):
                        '7': '--...', '8': '---..', '9': '----.',
                        '0': '-----',
 
-                       ' ': ' ', ',': '--..--', '.': '.-.-.-',
+                       ' ': ' ', '\n': '\n', ',': '--..--', '.': '.-.-.-',
                        '?': '..--..', '\'': '.----.', '/': '-..-.',
                        '-': '-....-', '(': '-.--.', ')': '-.--.-'}
 
@@ -25,3 +28,15 @@ def encrypt_to_morse(input_str):
             morse_str += morse_code_dict[c] + ' '
 
     return morse_str
+
+
+def morse_to_sound(morse_code):
+    for c in morse_code:
+        if c == '.':
+            os.system("morse_dot.mp3")
+            time.sleep(0.03)
+        elif c == '-':
+            os.system("morse_dash.mp3")
+            time.sleep(0.07)
+        elif c == ' ':
+            time.sleep(0.15)
